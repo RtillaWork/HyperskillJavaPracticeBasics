850e50b3bd3c00ca0ae9fc8283c5c8e0d002ee32
Naomi N <39413995+nauranan@users.noreply.github.com>
2021-11-08 02:56:18 -0700

    Update 2021nov080220.java

--- 2021nov080220.java
+++ 2021nov080220.java
@@ -6,10 +6,29 @@ public class Main {
         Queue<Integer> queue = new ArrayDeque<>(Arrays.asList(1, 2, 3, 4));
 
         // write your code here
-        queue.offer(5);
-        queue.poll();
-        queue.poll();
-
+//        queue.offer(5);
+//        queue.poll();
+//        queue.poll();
         System.out.println(queue);
+
+
+        Deque<String> states = new ArrayDeque<String>();
+
+        states.add("Germany");
+        states.add("France");
+        states.push("UK");
+        states.offerLast("Norway");
+
+        String sPop = states.pop();
+        String sPeek = states.peek();
+        String sPeekLast = states.peekLast();
+        states.offer(sPop);
+        String sPollLast = states.pollLast();
+
+        while (states.peek() != null) {
+            System.out.print(states.pop());
+        }
+
+        System.out.println(states);
     }
 }
