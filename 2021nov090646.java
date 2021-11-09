482b9ab0376288da4db716d24e8b4d28d59a0366
Naomi N <39413995+nauranan@users.noreply.github.com>
2021-11-09 06:49:08 -0700

    Update 2021nov090646.java

--- 2021nov090646.java
+++ 2021nov090646.java
@@ -43,6 +43,12 @@ class Utils {
     }
 }
 
+// class Utils {
+
+//     public static void sortUsers(List<User> users) {
+//         users.sort(Comparator.comparing(User::getName).thenComparing(User::getAge, Comparator.reverseOrder()));
+//     }
+
 
 // Multiple fields
 // You have a User class with two fields: String name and int age. Implement a method that takes a list of users, List<User>, and sorts it according to the following rule:
