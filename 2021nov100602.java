b3e3a09388bb466a1b1b7c038fa80323aa96cdaa
Naomi N <39413995+nauranan@users.noreply.github.com>
2021-11-10 06:47:20 -0700

    Update 2021nov100602.java

--- 2021nov100602.java
+++ 2021nov100602.java
@@ -12,13 +12,35 @@ class MinMax {
 
         // your implementation here
         List<? extends T> ts = stream.collect(Collectors.toList());
-        List<? extends T> dupTs = new ArrayList<>(ts);
 
         minMaxConsumer.accept(ts.stream().min(order).orElse(null),
-                dupTs.stream().max(order).orElse(null));
+                ts.stream().max(order).orElse(null));
 
     }
 }
+
+
+// import java.util.*;
+// import java.util.function.*;
+// import java.util.stream.*;
+
+
+// class MinMax {
+
+//     public static <T> void findMinMax(
+//             Stream<? extends T> stream,
+//             Comparator<? super T> order,
+//             BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+//         // your implementation here
+//         List<? extends T> ts = stream.collect(Collectors.toList());
+//         List<? extends T> dupTs = new ArrayList<>(ts);
+
+//         minMaxConsumer.accept(ts.stream().min(order).orElse(null),
+//                 dupTs.stream().max(order).orElse(null));
+
+//     }
+// }
 // Implement a method for finding min and max elements of a stream in accordance with a given comparator.
 
 // The found elements pass to minMaxConsumer in the following way:
