7d0eedda975921976dedfc36313900b97cec0307
Naomi N <39413995+nauranan@users.noreply.github.com>
2021-11-10 07:52:56 -0700

    Update 2021nov100742.java

--- 2021nov100742.java
+++ 2021nov100742.java
@@ -6,6 +6,7 @@ class StreamUtils {
 
     public static Stream<Integer> generateStreamWithPowersOfTwo(int n) {
 //         return Stream.iterate(0, p += 1).map(p -> (int) Math.pow(2, p)).limit(n); // replace it with your code
-            return Stream.iterate(1, p -> p *= 2).limit(n); 
+//             return Stream.iterate(1, p -> p *= 2).limit(n); 
+         return Stream.iterate(1, p -> p * 2).limit(n); 
     }
 }
