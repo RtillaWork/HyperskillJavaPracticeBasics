c8725e8753a3559fcab1501c52a9c3e5f6bee7fa
Naomi N <39413995+nauranan@users.noreply.github.com>
2021-11-12 05:34:01 -0700

    Create 20021nov120518.java

---
+++ 20021nov120518.java
@@ -0,0 +1,44 @@
+// sorted and divisble by both 3 and 5, skipping the first 2
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.*;
+
+class EvenAndOddFilter {
+
+    public static IntStream createFilteringStream(IntStream evenStream, IntStream oddStream) {
+        return IntStream.concat(evenStream, oddStream)
+                .sorted()
+                .filter(n -> n % 3 == 0 && n % 5 == 0)
+                .skip(2); // write your stream here
+    }
+
+    // Don't change the code below
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] values = scanner.nextLine().split(" ");
+
+        List<Integer> numbers = Arrays.stream(values)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int[] evenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(x -> x)
+                .toArray();
+
+        int[] oddNumbers = numbers.stream()
+                .filter(n -> n % 2 == 1)
+                .mapToInt(x -> x)
+                .toArray();
+
+        IntStream filteringStream = createFilteringStream(
+                Arrays.stream(evenNumbers),
+                Arrays.stream(oddNumbers));
+
+        System.out.println(filteringStream.boxed().collect(Collectors.toList()));
+    }
+}
